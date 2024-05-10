f2b91a1cbb82304210b2a6f876299fbb72f64ab5
EC2 Default User <takizawasayuki0912@gmail.com>
2024-05-10 07:17:21 +0000

    [Finish] 8章

--- db/migrate/20240505055943_create_active_storage_tables.active_storage.rb
+++ db/migrate/20240505055943_create_active_storage_tables.active_storage.rb
@@ -23,6 +23,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.references :blob,     null: false, type: foreign_key_type
 
       t.datetime :created_at, null: false
+      
 
       t.index [ :record_type, :record_id, :name, :blob_id ], name: "index_active_storage_attachments_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
